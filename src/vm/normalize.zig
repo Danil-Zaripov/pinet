d32bc4552321a3a5d849e3214b563eb792bcb671
kogora <georgy.sichkar@gmail.com>
2026-09-14 21:15:16 +0300

    Enhance code style

--- src/vm/normalize.zig
+++ src/vm/normalize.zig
@@ -1,7 +1,9 @@
 const std = @import("std");
-const Core = @import("core.zig");
 
-const Types = @import("shared_runtime").Types;
+const Runtime = @import("shared_runtime");
+const Memory = Runtime.Memory;
+const EquationFetcher = Runtime.EquationFetcher;
+const Types = Runtime.Types;
 const Name = Types.Name;
 const Agent = Types.Agent;
 const Equation = Types.Equation;
@@ -10,15 +12,15 @@ const EquationUnnormalized = Types.EquationUnnormalized;
 const Debug = @import("debug");
 const Config = @import("config");
 
-fn name_name(c: *Core, lname: *Name, rname: *Name) !?Equation {
+fn name_name(name_heap: Memory.Heap(Name), lname: *Name, rname: *Name) !?Equation {
     //Debug.log(.print_interactions, "name - name interaction\n", .{});
 
-    const ltraversed = lname.traverseFree(c.local_ctx.name_heap);
-    const rtraversed = rname.traverseFree(c.local_ctx.name_heap);
+    const ltraversed = lname.traverseFree(name_heap);
+    const rtraversed = rname.traverseFree(name_heap);
     if (ltraversed.port) |lport| {
-        defer c.local_ctx.name_heap.freeOne(ltraversed);
+        defer name_heap.freeOne(ltraversed);
         if (rtraversed.port) |rport| {
-            defer c.local_ctx.name_heap.freeOne(rtraversed);
+            defer name_heap.freeOne(rtraversed);
             return Equation{ .lhs = lport.agent, .rhs = rport.agent };
         } else {
             rtraversed.port = lport;
@@ -29,15 +31,15 @@ fn name_name(c: *Core, lname: *Name, rname: *Name) !?Equation {
     return null;
 }
 
-fn name_agent(c: *Core, name: *Name, agent: *Agent) !?Equation {
+fn name_agent(name_heap: Memory.Heap(Name), name: *Name, agent: *Agent) !?Equation {
     // TODO (KoGora): perf analysis
     if (Config.debug_printing.print_interactions) {
         //std.debug.print("{s} - name interaction\n", .{c.runtime.agent_id_map.findKey(agent.id).?});
     }
 
-    const traversed = name.traverseFree(c.local_ctx.name_heap);
+    const traversed = name.traverseFree(name_heap);
     if (traversed.port) |port| {
-        defer c.local_ctx.name_heap.freeOne(traversed);
+        defer name_heap.freeOne(traversed);
         return Equation{ .lhs = port.agent, .rhs = agent };
     } else {
         traversed.port = .{ .agent = agent };
@@ -45,15 +47,27 @@ fn name_agent(c: *Core, name: *Name, agent: *Agent) !?Equation {
     return null;
 }
 
-pub fn normalizeEquation(c: *Core, eq: EquationUnnormalized) !?Equation {
+pub fn pushEquation(name_heap: Memory.Heap(Name), equation_fetcher: EquationFetcher, eq: EquationUnnormalized) !void {
+    if (try normalizeEquation(name_heap, eq)) |normalized| {
+        try equation_fetcher.push(normalized);
+    }
+}
+
+pub fn pushUrgentEquation(name_heap: Memory.Heap(Name), equation_fetcher: EquationFetcher, eq: EquationUnnormalized) !void {
+    if (try normalizeEquation(name_heap, eq)) |normalized| {
+        try equation_fetcher.pushUrgent(normalized);
+    }
+}
+
+pub fn normalizeEquation(name_heap: Memory.Heap(Name), eq: EquationUnnormalized) !?Equation {
     switch (eq.lhs) {
         .name => |lname| {
             switch (eq.rhs) {
                 .name => |rname| {
-                    return try name_name(c, lname, rname);
+                    return try name_name(name_heap, lname, rname);
                 },
                 .agent => |ragent| {
-                    return try name_agent(c, lname, ragent);
+                    return try name_agent(name_heap, lname, ragent);
                 },
                 else => unreachable,
             }
@@ -61,7 +75,7 @@ pub fn normalizeEquation(c: *Core, eq: EquationUnnormalized) !?Equation {
         .agent => |lagent| {
             switch (eq.rhs) {
                 .name => |rname| {
-                    return try name_agent(c, rname, lagent);
+                    return try name_agent(name_heap, rname, lagent);
                 },
                 .agent => |ragent| {
                     return Equation{ .lhs = lagent, .rhs = ragent };
